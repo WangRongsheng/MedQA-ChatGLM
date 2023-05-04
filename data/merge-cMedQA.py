4fd1ca95c24f783472c3a2f427667299e4b5e7cc
MPU王荣胜 <fujingshusheng@gmail.com>
2023-05-04 18:55:50 +0800

    add merge-cMedQA

---
+++ data/merge-cMedQA.py
@@ -0,0 +1,13 @@
+import json
+
+# 读取第一个JSON文件
+with open('cMedQA.json', 'r') as f1:
+    lst1 = json.load(f1)
+# 读取第二个JSON文件
+with open('cMedQA2.json', 'r') as f2:
+    dict2 = json.load(f2)
+# 将字典dict2合并到lst1中
+lst1.append(dict2)
+# 将合并后的数据写入新的JSON文件
+with open('merged.json', 'w') as f:
+    json.dump(lst1, f, indent=4)
