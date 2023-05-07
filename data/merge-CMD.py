7e38fb4b169d23e0886a6b3159c4923952439560
MPU王荣胜 <fujingshusheng@gmail.com>
2023-05-07 18:42:44 +0800

    update CMD

--- data/merge-CMD.py
+++ data/merge-CMD.py
@@ -1,22 +1,13 @@
 import json
-import os
 
-# 获取当前文件夹下所有.json文件路径
-file_list = []
-for file in os.listdir():
-    if file.endswith('.json'):
-        file_list.append(file)
-
-# 新建合并后的字典
-merged_data = []
-# 逐个读取json文件并合并到merged_data中
-for file in file_list:
-    with open(file, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        
-        merged_data.append(data)
-
-# 写入新的json文件
-with open('merged-CMD.json', 'w', encoding='utf-8') as f:
-    json.dump(merged_data, f, ensure_ascii=False, indent=4)
-print('合并完成！')
+# 读取第一个JSON文件
+with open('merged4.json', 'r') as f1:
+    lst1 = json.load(f1)
+# 读取第二个JSON文件
+with open('zhongliu.json', 'r') as f2:
+    dict2 = json.load(f2)
+# 将字典dict2合并到lst1中
+lst1.append(dict2)
+# 将合并后的数据写入新的JSON文件
+with open('merged-CMD.json', 'w') as f:
+    json.dump(lst1, f, indent=4)
